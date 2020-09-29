70e512c23570c29ffa4fbb94319a4b608c2777cc
GuyM1234 <63429285+GuyM1234@users.noreply.github.com>
2020-09-30 00:37:21 +0300

    Update Achtung.py

--- Achtung.py
+++ Achtung.py
@@ -5,6 +5,7 @@ import math
 from time import sleep
 import copy
 import datetime
+from player import player
 
 pygame.init()
 width = 1200
@@ -23,40 +24,6 @@ GREEN = (0,255,0)
 color_list = [BLUE, ORNAGE, RED, GREEN, WHITE]
 SQUARESIZE = 100
 
-class player(object):
-    def __init__(self,num_player,move):
-        self.color = color_list[num_player]
-        self.width = 4
-        self.dir = random.randint(1,360)
-        self.posx = random.randint(200,700)
-        self.posy = random.randint(200,700)  
-        self.is_playing = True
-        self.points = 0
-        self.left= move[0]
-        self.right = move[1]
-        self.mult_forward_move = self.width + 2
-
-    def legal_move(self, screen):
-        if self.is_border_touched():
-            return False        
-        posx = self.posx + math.cos(self.dir) * self.mult_forward_move
-        posy = self.posy + math.sin(self.dir) * self.mult_forward_move
-        color = screen.get_at((round(posx),round(posy)))
-        if color != BLACK:
-            return False
-        return True
-
-    def move(self):
-        self.posx += math.cos(self.dir)
-        self.posy += math.sin(self.dir)
-
-    def update_dir(self, num):
-        self.dir += num
-
-    def is_border_touched(self):
-        if self.posx > 895 or self.posx < 5 or self.posy > 895 or self.posy < 5:
-            return True
-        return False
 
 class ability(object):
     def __init__(self,posx,posy):
@@ -120,7 +87,7 @@ def update_players_dirs(playing_list, dir_list):
 def create_players(move_list):
     player_list = []
     for i in range(len(players_index)):
-        player_list.append(player(players_index[i], move_list[i]))
+        player_list.append(player(move_list[i], color_list[players_index[i]]))
     return player_list
 
 def update_scores(scores, player_list):
