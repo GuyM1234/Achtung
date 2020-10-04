b40bd6f0b284df3db4f5a3cb20e5c0f2e1d77ada
RoyM1234 <rmainfeld1@gmail.com>
2020-10-04 17:39:24 +0300

    Update player.py

--- player.py
+++ player.py
@@ -23,16 +23,16 @@ class player(object):
     def legal_move(self, screen):
         if self.is_border_touched():
             return False        
-        posx = self.posx + math.cos(self.dir) * self.mult_forward_move
-        posy = self.posy + math.sin(self.dir) * self.mult_forward_move
+        posx = self.posx + math.cos(self.dir) * self.mult_forward_move  
+        posy = self.posy + math.sin(self.dir) * self.mult_forward_move 
         color = screen.get_at((round(posx),round(posy)))
         if color != BLACK:
             return False
         return True
 
     def move(self):
-        self.posx += math.cos(self.dir)
-        self.posy += math.sin(self.dir)
+        self.posx += self.move_times * math.cos(self.dir)
+        self.posy += self.move_times * math.sin(self.dir)
 
     def update_dir(self):
         self.dir += self.update_dir_value
